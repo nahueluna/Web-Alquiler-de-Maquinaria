7daa4308b9adf95142b5ba584e3cc2bc438555e2
RamosThiago <thiagoramosnoilhan@gmail.com>
2025-05-14 16:49:09 -0300

    Estructura basica del frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./features/Home/Home.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Home />
+  </StrictMode>
+);
