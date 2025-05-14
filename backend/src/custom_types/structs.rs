de21ec7fcdab93da5fbba0092f49e8dc34998e7f
nahuel <nahuelnicolasluna@hotmail.com>
2025-05-13 22:30:15 -0300

    Se agrega estructura para recibir requests de login

--- backend/src/custom_types/structs.rs
+++ backend/src/custom_types/structs.rs
@@ -1,9 +1,17 @@
+use chrono::{Duration, Utc};
 use serde::{Deserialize, Serialize};
 
 // the input to our `create_user` handler
 #[derive(Deserialize)]
 pub struct CreateUser {
     pub username: String,
+}
+
+// the input to our `client_login` handler
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
 }
 
 // the output to our `create_user` handler
