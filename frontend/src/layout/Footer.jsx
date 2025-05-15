6fc015adb99329d6c160c14e287220570a9f8753
Noxde <mateo.tesorieri@hotmail.com>
2025-05-15 13:29:24 -0300

    Added a simple footer

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link, Sheet, Typography } from "@mui/joy/";
+import Logo from "../assets/LogoCompleto.png";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <Sheet
+      sx={{
+        padding: 2,
+        backgroundColor: "#dfe1e3",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <a href="/">
+          <img width={"200px"} src={Logo} alt="" />
+        </a>
+      </div>
+      <Sheet sx={{ backgroundColor: "transparent" }}>
+        <Typography level="title-lg">Contacto</Typography>
+
+        <Typography level="body-sm">
+          <Link href="mailto:soporte@alquilador.com" variant="plain">
+            soporte@alquilador.com
+          </Link>
+        </Typography>
+        <Typography level="body-sm">
+          <Link href="tel:+888888888" variant="plain">
+            +888888888
+          </Link>
+        </Typography>
+      </Sheet>
+    </Sheet>
+  );
 };
 
 export default Footer;
