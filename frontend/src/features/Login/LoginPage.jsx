7b83b38e105e0c06c79ae8e609b24d3df4021fa1
RamosThiago <thiagoramosnoilhan@gmail.com>
2025-05-15 13:53:12 -0300

    Borrar css

--- frontend/src/features/Login/LoginPage.jsx
+++ frontend/src/features/Login/LoginPage.jsx
@@ -1,4 +1,5 @@
 import Button from "@mui/joy/Button";
+import CssBaseline from "@mui/joy/CssBaseline";
 import FormHelperText from "@mui/joy/FormHelperText";
 import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
@@ -6,7 +7,7 @@ import Option from "@mui/joy/Option";
 import Select from "@mui/joy/Select";
 import Sheet from "@mui/joy/Sheet";
 import Stack from "@mui/joy/Stack";
-import { useColorScheme } from "@mui/joy/styles";
+import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import Typography from "@mui/joy/Typography";
 import { useFormik } from "formik";
 import * as React from "react";
@@ -64,55 +65,63 @@ export default function LoginPage(props) {
     },
   });
   return (
-    <Sheet
-      variant="outlined"
-      sx={{ p: 4, borderRadius: "md", width: 400, mx: "auto", mt: 6 }}
-    >
-      <Typography level="h4" mb={2}>
-        Iniciar sesión
-      </Typography>
+    <main>
+      <CssVarsProvider>
+        <CssBaseline />
+      </CssVarsProvider>
 
-      <form onSubmit={formik.handleSubmit}>
-        <Stack spacing={2}>
-          <div>
-            <FormLabel>Email</FormLabel>
-            <Input
-              name="email"
-              type="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-            />
-            {formik.touched.email && formik.errors.email && (
-              <FormHelperText color="danger">
-                {formik.errors.email}
-              </FormHelperText>
-            )}
-          </div>
+      <Sheet
+        variant="outlined"
+        sx={{ p: 4, borderRadius: "md", width: 400, mx: "auto", mt: 6 }}
+      >
+        <Typography level="h4" mb={2}>
+          Iniciar sesión
+        </Typography>
 
-          <div>
-            <FormLabel>Contraseña</FormLabel>
-            <Input
-              name="password"
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.password && Boolean(formik.errors.password)}
-            />
-            {formik.touched.password && formik.errors.password && (
-              <FormHelperText color="danger">
-                {formik.errors.password}
-              </FormHelperText>
-            )}
-          </div>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={2}>
+            <div>
+              <FormLabel>Email</FormLabel>
+              <Input
+                name="email"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <FormHelperText color="danger">
+                  {formik.errors.email}
+                </FormHelperText>
+              )}
+            </div>
 
-          <Button type="submit" variant="solid" color="primary">
-            Ingresar
-          </Button>
-        </Stack>
-      </form>
-    </Sheet>
+            <div>
+              <FormLabel>Contraseña</FormLabel>
+              <Input
+                name="password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+              />
+              {formik.touched.password && formik.errors.password && (
+                <FormHelperText color="danger">
+                  {formik.errors.password}
+                </FormHelperText>
+              )}
+            </div>
+
+            <Button type="submit" variant="solid" color="primary">
+              Ingresar
+            </Button>
+          </Stack>
+        </form>
+      </Sheet>
+    </main>
   );
 }
