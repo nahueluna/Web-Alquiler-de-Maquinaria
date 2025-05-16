faadffbd6678d1204625a182d60dba4005d32072
RamosThiago <thiagoramosnoilhan@gmail.com>
2025-05-15 23:30:35 -0300

    Cambios en navbar

--- frontend/src/features/Login/LoginPage.jsx
+++ frontend/src/features/Login/LoginPage.jsx
@@ -68,60 +68,65 @@ export default function LoginPage(props) {
     <main>
       <CssVarsProvider>
         <CssBaseline />
+        <Sheet
+          variant="outlined"
+          sx={{
+            p: 4,
+            borderRadius: "md",
+            width: 400,
+            mx: "auto",
+            mt: 6,
+          }}
+        >
+          <Typography level="h4" mb={2}>
+            Iniciar sesión
+          </Typography>
+
+          <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={2}>
+              <div>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  name="email"
+                  type="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                />
+                {formik.touched.email && formik.errors.email && (
+                  <FormHelperText color="danger">
+                    {formik.errors.email}
+                  </FormHelperText>
+                )}
+              </div>
+
+              <div>
+                <FormLabel>Contraseña</FormLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                />
+                {formik.touched.password && formik.errors.password && (
+                  <FormHelperText color="danger">
+                    {formik.errors.password}
+                  </FormHelperText>
+                )}
+              </div>
+
+              <Button type="submit" variant="solid" color="primary">
+                Ingresar
+              </Button>
+            </Stack>
+          </form>
+        </Sheet>
       </CssVarsProvider>
-
-      <Sheet
-        variant="outlined"
-        sx={{ p: 4, borderRadius: "md", width: 400, mx: "auto", mt: 6 }}
-      >
-        <Typography level="h4" mb={2}>
-          Iniciar sesión
-        </Typography>
-
-        <form onSubmit={formik.handleSubmit}>
-          <Stack spacing={2}>
-            <div>
-              <FormLabel>Email</FormLabel>
-              <Input
-                name="email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-              />
-              {formik.touched.email && formik.errors.email && (
-                <FormHelperText color="danger">
-                  {formik.errors.email}
-                </FormHelperText>
-              )}
-            </div>
-
-            <div>
-              <FormLabel>Contraseña</FormLabel>
-              <Input
-                name="password"
-                type="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={
-                  formik.touched.password && Boolean(formik.errors.password)
-                }
-              />
-              {formik.touched.password && formik.errors.password && (
-                <FormHelperText color="danger">
-                  {formik.errors.password}
-                </FormHelperText>
-              )}
-            </div>
-
-            <Button type="submit" variant="solid" color="primary">
-              Ingresar
-            </Button>
-          </Stack>
-        </form>
-      </Sheet>
     </main>
   );
 }
