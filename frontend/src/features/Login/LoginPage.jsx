a3ab5fdd2e3ba7a1188b445c13e5687c9c345761
Noxde <mateo.tesorieri@hotmail.com>
2025-05-15 23:01:05 -0300

    Changed the yup email check for a regex

--- frontend/src/features/Login/LoginPage.jsx
+++ frontend/src/features/Login/LoginPage.jsx
@@ -12,7 +12,10 @@ export default function LoginPage() {
   const validationSchema = yup.object({
     email: yup
       .string("Enter your email")
-      .email("Enter a valid email")
+      .matches(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Enter a valid email"
+      )
       .required("Email is required"),
     password: yup
       .string("Enter your password")
