f3c9ff05f43dd772e0fdac36e3ce444f60586f9d
Noxde <mateo.tesorieri@hotmail.com>
2025-05-15 23:00:37 -0300

    Removed unused function and unnecessary code

--- frontend/src/features/Login/LoginPage.jsx
+++ frontend/src/features/Login/LoginPage.jsx
@@ -1,48 +1,14 @@
 import Button from "@mui/joy/Button";
-import CssBaseline from "@mui/joy/CssBaseline";
 import FormHelperText from "@mui/joy/FormHelperText";
 import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
-import Option from "@mui/joy/Option";
-import Select from "@mui/joy/Select";
 import Sheet from "@mui/joy/Sheet";
 import Stack from "@mui/joy/Stack";
-import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import Typography from "@mui/joy/Typography";
 import { useFormik } from "formik";
-import * as React from "react";
 import * as yup from "yup";
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
-  const [mounted, setMounted] = React.useState(false);
-
-  // necessary for server-side rendering
-  // because mode is undefined on the server
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
-  if (!mounted) {
-    return <Button variant="soft">Change mode</Button>;
-  }
-
-  return (
-    <Select
-      variant="soft"
-      value={mode}
-      onChange={(event, newMode) => {
-        setMode(newMode);
-      }}
-      sx={{ width: "max-content" }}
-    >
-      <Option value="system">System</Option>
-      <Option value="light">Light</Option>
-      <Option value="dark">Dark</Option>
-    </Select>
-  );
-}
-
-export default function LoginPage(props) {
+export default function LoginPage() {
   const validationSchema = yup.object({
     email: yup
       .string("Enter your email")
@@ -65,63 +31,60 @@ export default function LoginPage(props) {
     },
   });
   return (
-    <main>
-      <CssVarsProvider>
-        <CssBaseline />
-      </CssVarsProvider>
+    <Sheet
+      variant="outlined"
+      sx={{
+        p: 4,
+        borderRadius: "md",
+        width: 400,
+        mx: "auto",
+      }}
+    >
+      <Typography level="h4" mb={2}>
+        Iniciar sesión
+      </Typography>
 
-      <Sheet
-        variant="outlined"
-        sx={{ p: 4, borderRadius: "md", width: 400, mx: "auto", mt: 6 }}
-      >
-        <Typography level="h4" mb={2}>
-          Iniciar sesión
-        </Typography>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack spacing={2}>
+          <div>
+            <FormLabel>Email</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+            />
+            {formik.touched.email && formik.errors.email && (
+              <FormHelperText color="danger">
+                {formik.errors.email}
+              </FormHelperText>
+            )}
+          </div>
 
-        <form onSubmit={formik.handleSubmit}>
-          <Stack spacing={2}>
-            <div>
-              <FormLabel>Email</FormLabel>
-              <Input
-                name="email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={formik.touched.email && Boolean(formik.errors.email)}
-              />
-              {formik.touched.email && formik.errors.email && (
-                <FormHelperText color="danger">
-                  {formik.errors.email}
-                </FormHelperText>
-              )}
-            </div>
+          <div>
+            <FormLabel>Contraseña</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <FormHelperText color="danger">
+                {formik.errors.password}
+              </FormHelperText>
+            )}
+          </div>
 
-            <div>
-              <FormLabel>Contraseña</FormLabel>
-              <Input
-                name="password"
-                type="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                error={
-                  formik.touched.password && Boolean(formik.errors.password)
-                }
-              />
-              {formik.touched.password && formik.errors.password && (
-                <FormHelperText color="danger">
-                  {formik.errors.password}
-                </FormHelperText>
-              )}
-            </div>
-
-            <Button type="submit" variant="solid" color="primary">
-              Ingresar
-            </Button>
-          </Stack>
-        </form>
-      </Sheet>
-    </main>
+          <Button type="submit" variant="solid" color="primary">
+            Ingresar
+          </Button>
+        </Stack>
+      </form>
+    </Sheet>
   );
 }
