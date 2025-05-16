b66cbb22ea24f7283c11a5a9479e48779f5ef999
Noxde <mateo.tesorieri@hotmail.com>
2025-05-15 22:56:40 -0300

    Removed unnecessary prop

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-const MainLayout = ({ children }) => (
+const MainLayout = () => (
   <>
     <Navbar />
     <Outlet />
