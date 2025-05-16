123b58628d612d791dbc4eb471e2aa219b796810
Noxde <mateo.tesorieri@hotmail.com>
2025-05-16 16:07:34 -0300

    Fix for MainLayout grid

--- frontend/src/layout/Navbar.jsx
+++ frontend/src/layout/Navbar.jsx
@@ -3,43 +3,45 @@ import SoloLogo from "../assets/SoloLogo.png";
 
 const Navbar = () => {
   return (
-    <Sheet
-      sx={{
-        padding: 2,
-        backgroundColor: "white",
-        display: "flex",
-        justifyContent: "space-between",
-        height: "50px",
-        alignItems: "center",
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 1000,
-      }}
-    >
-      <Link href="/">
-        <img width={"50px"} src={SoloLogo} alt="" />
-      </Link>
-      <Input variant="outlined" placeholder="Buscar maquinaria..."></Input>
-      <Button
-        color="danger"
-        onClick={function () {}}
-        size="sm"
-        variant="outlined"
+    <div>
+      <Sheet
+        sx={{
+          padding: 2,
+          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "space-between",
+          height: "50px",
+          alignItems: "center",
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1000,
+        }}
       >
-        Registrarse
-      </Button>
-      <Button
-        color="danger"
-        onClick={function () {}}
-        size="sm"
-        variant="outlined"
-      >
-        Iniciar sesion
-      </Button>
-    </Sheet>
+        <Link href="/">
+          <img width={"50px"} src={SoloLogo} alt="" />
+        </Link>
+        <Input variant="outlined" placeholder="Buscar maquinaria..."></Input>
+        <Button
+          color="danger"
+          onClick={function () {}}
+          size="sm"
+          variant="outlined"
+        >
+          Registrarse
+        </Button>
+        <Button
+          color="danger"
+          onClick={function () {}}
+          size="sm"
+          variant="outlined"
+        >
+          Iniciar sesion
+        </Button>
+      </Sheet>
+    </div>
   );
 };
 
