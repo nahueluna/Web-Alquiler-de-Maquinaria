21cfd53862132cef70f63735979dd82a88033b76
Noxde <mateo.tesorieri@hotmail.com>
2025-05-15 22:57:10 -0300

    Used a grid to make the layout

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { Box } from "@mui/joy";
 
 const MainLayout = () => (
   <>
-    <Navbar />
-    <Outlet />
-    <Footer />
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "grid",
+        gridTemplateRows: "auto 1fr auto", // navbar | page content | footer
+      }}
+    >
+      <Navbar />
+      <Box
+        sx={{
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Outlet />
+      </Box>
+      <Footer />
+    </Box>
   </>
 );
 
