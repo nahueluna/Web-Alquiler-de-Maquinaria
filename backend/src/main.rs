a34084f9213718ca589259b053695f22b52557be
nahuel <nahuelnicolasluna@hotmail.com>
2025-05-23 20:26:47 -0300

    CORS restrictions configured

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,7 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use tower_http::cors::{Any, CorsLayer};
 
 use handlers::auth::{client_login, client_sign_up, root};
 use lettre::transport::smtp::client;
@@ -22,7 +23,13 @@ async fn main() {
         // `POST /register` goes to `client_sign_up`
         .route("/signup", post(client_sign_up))
         // `POST /login` goes to `client_login`
-        .route("/login", post(client_login));
+        .route("/login", post(client_login))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(vec!["http://localhost:5173".parse().unwrap()]) // Allow all origins; restrict in production!
+                .allow_methods([axum::http::Method::GET, axum::http::Method::POST])
+                .allow_headers([axum::http::header::CONTENT_TYPE]),
+        );
 
     // run our app with hyper, listening globally on port 8000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
