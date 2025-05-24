dc9bb0134f41d4626c2c09a692b73b4aefb4f70e
Valentino Amato <105114984+valentinoamato@users.noreply.github.com>
2025-05-24 14:43:46 -0300

    Create table machines

--- backend/createdb.sql
+++ backend/createdb.sql
@@ -1,3 +1,6 @@
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email varchar(254) UNIQUE NOT NULL,
@@ -8,4 +11,16 @@ CREATE TABLE users (
     phone varchar(50) NULL,
     password varchar(100) NOT NULL,
     role smallint NOT NULL
+);
+
+CREATE TABLE machines (
+    id SERIAL PRIMARY KEY,
+    serial_number varchar(50) UNIQUE NOT NULL,
+    name varchar(150) NOT NULL,
+    brand varchar(50) NOT NULL,
+    model varchar(50) NOT NULL,
+    year smallint NOT NULL,
+    policy varchar(300) NOT NULL,
+    description varchar(500) NOT NULL,
+    price real NOT NULL
 );
