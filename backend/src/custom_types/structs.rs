ec0e5cbbcd2c5ff0ed2db3d836783b3231c207b3
nahuel <nahuelnicolasluna@hotmail.com>
2025-05-24 20:22:46 -0300

    Added catalog parameters and machinery structs

--- backend/src/custom_types/structs.rs
+++ backend/src/custom_types/structs.rs
@@ -1,5 +1,7 @@
 use chrono::{Duration, Utc};
-use serde::{Deserialize, Serialize};
+use deadpool_postgres::Pool;
+use serde::{de, Deserialize, Serialize};
+use sha2::digest::Mac;
 use std::sync::Arc;
 use validator::Validate;
 
@@ -48,4 +50,33 @@ pub struct CatalogParams {
     pub min_price: Option<f32>,
     #[validate(range(min = 0.0))]
     pub max_price: Option<f32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Machine {
+    pub id: i32,
+    pub serial_number: String,
+    pub name: String,
+    pub brand: String,
+    pub model: String,
+    pub year: i16,
+    pub policy: String,
+    pub description: String,
+    pub price: f32,
+}
+
+impl Machine {
+    pub fn build_from_row(row: &tokio_postgres::Row) -> Self {
+        Machine {
+            id: row.get("id"),
+            serial_number: row.get("serial_number"),
+            name: row.get("name"),
+            brand: row.get("brand"),
+            model: row.get("model"),
+            year: row.get("year"),
+            policy: row.get("policy"),
+            description: row.get("description"),
+            price: row.get("price"),
+        }
+    }
 }
