5318cd446cc2a00c179b876e958cb17526458be1
RamosThiago <thiagoramosnoilhan@gmail.com>
2025-05-25 00:54:45 -0300

    Updated AppRoutes and gitignore

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
+import Dashboard from "../features/Dashboard/DashboardPage";
 import ExplorePage from "../features/Explore/ExplorePage";
 import Home from "../features/Home/HomePage";
 import Login from "../features/Login/LoginPage";
 import NotFoundPage from "../features/NotFoundPage";
 import Product from "../features/Product/Product";
+import Profile from "../features/Profile/Profile";
+import RecoverPassword from "../features/RecoverPassword/RecoverPassword";
 import RegisterPage from "../features/Register/RegisterPage";
 import Terms from "../features/Terms/TermsPage";
 import TwoFactor from "../features/TwoFactorAuth/TwoFactor";
 import MainLayout from "../layout/MainLayout";
-import RecoverPassword from "../features/RecoverPassword/RecoverPassword"
-import Profile from "../features/Profile/Profile";
-
 
 export default function AppRoutes() {
   return (
@@ -24,6 +24,7 @@ export default function AppRoutes() {
         <Route path="/terms" element={<Terms />} />
         <Route path="/recover-password" element={<RecoverPassword />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Route>
       <Route path="/two-factor" element={<TwoFactor />} />
 
