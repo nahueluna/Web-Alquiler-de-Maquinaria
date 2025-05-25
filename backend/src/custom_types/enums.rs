cdf8492deb4ce9e272da5a90c3dc401674e3920a
nahuel <nahuelnicolasluna@hotmail.com>
2025-05-25 03:51:18 -0300

    Added order by enums

--- backend/src/custom_types/enums.rs
+++ backend/src/custom_types/enums.rs
@@ -1,4 +1,20 @@
+use serde::Deserialize;
+
 pub enum RunningEnv {
     Production,
     Testing,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum OrderByField {
+    Price,
+    Rating,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum OrderDirection {
+    Asc,
+    Desc,
 }
