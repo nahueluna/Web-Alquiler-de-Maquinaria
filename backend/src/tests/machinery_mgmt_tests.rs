a82b82072564bd0292489dce659000df25372a84
nahuel <nahuelnicolasluna@hotmail.com>
2025-05-25 03:55:19 -0300

    The explore catalog test now includes a filtering and sorting test case

--- backend/src/tests/machinery_mgmt_tests.rs
+++ backend/src/tests/machinery_mgmt_tests.rs
@@ -9,11 +9,11 @@ mod tests {
         setup().await;
         let http_client = Client::new();
 
-        // ----------- Page 1, Page size 3, valid request without filters
+        // ----------- Page 1, Page size 2, valid request without filters
 
         let basic_page1_res = http_client
             .get("http://localhost:8000/explore")
-            .query(&[("page", "1"), ("page_size", "3")])
+            .query(&[("page", "1"), ("page_size", "2")])
             .send()
             .await
             .unwrap();
@@ -22,22 +22,19 @@ mod tests {
 
         let response_body = basic_page1_res.json::<serde_json::Value>().await.unwrap();
 
-        assert_eq!(response_body["items"].as_array().length(), Some(3));
+        assert_eq!(response_body["items"].as_array().length(), Some(2));
 
         let machine1 = &response_body["items"][0];
         let machine2 = &response_body["items"][1];
 
         assert_eq!(machine1.as_object().unwrap().get("id").unwrap(), 1);
-        assert_eq!(
-            machine2.as_object().unwrap().get("serial_number").unwrap(),
-            "SN1002"
-        );
+        assert_eq!(machine2.as_object().unwrap().get("model").unwrap(), "310SL");
 
-        // ----------- Page 2, Page size 3, valid request with filters
+        // ----------- Page 2, Page size 2, valid request with filters
 
         let basic_page2_res = http_client
             .get("http://localhost:8000/explore")
-            .query(&[("page", "2"), ("page_size", "3")])
+            .query(&[("page", "2"), ("page_size", "2")])
             .send()
             .await
             .unwrap();
@@ -46,15 +43,44 @@ mod tests {
 
         let response_body = basic_page2_res.json::<serde_json::Value>().await.unwrap();
 
-        assert_eq!(response_body["items"].as_array().length(), Some(3));
+        assert_eq!(response_body["items"].as_array().length(), Some(2));
 
-        let machine1 = &response_body["items"][0];
-        let machine2 = &response_body["items"][1];
+        let machine3 = &response_body["items"][0];
+        let machine4 = &response_body["items"][1];
 
-        assert_eq!(machine1.as_object().unwrap().get("id").unwrap(), 4);
+        assert_eq!(machine3.as_object().unwrap().get("id").unwrap(), 3);
+        assert_eq!(machine4.as_object().unwrap().get("model").unwrap(), "E35");
+
+        // ----------- Page default, Page size 5, valid request with search term, minimum price and order
+
+        let basic_page2_res = http_client
+            .get("http://localhost:8000/explore")
+            .query(&[
+                ("page", "1"),
+                ("page_size", "5"),
+                ("search", "excavadora"),
+                ("min_price", "80000"),
+                ("order_by", "price"),
+                ("order_dir", "asc"),
+            ])
+            .send()
+            .await
+            .unwrap();
+
+        assert_eq!(basic_page2_res.status(), 200);
+
+        let response_body = basic_page2_res.json::<serde_json::Value>().await.unwrap();
+
+        assert_eq!(response_body["items"].as_array().length(), Some(2));
+
+        let machine5 = &response_body["items"][0];
+        let machine6 = &response_body["items"][1];
+
+        assert_eq!(machine5.as_object().unwrap().get("id").unwrap(), 2);
         assert_eq!(
-            machine2.as_object().unwrap().get("serial_number").unwrap(),
-            "SN1005"
+            machine6.as_object().unwrap().get("model").unwrap(),
+            "CAT320D"
         );
+
     }
 }
