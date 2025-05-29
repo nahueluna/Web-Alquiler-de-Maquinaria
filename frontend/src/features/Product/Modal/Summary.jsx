134d08a71ce4a8753153853f7b471940927ce74a
Noxde <mateo.tesorieri@hotmail.com>
2025-05-29 01:36:29 -0300

    Add Summary component

---
+++ frontend/src/features/Product/Modal/Summary.jsx
@@ -0,0 +1,44 @@
+import { Table, Typography } from "@mui/joy";
+
+function Summary({ info }) {
+  const { machine, selectedLocation, dates, days } = info;
+  const { name, model, price } = machine;
+
+  return (
+    <>
+      <Table
+        sx={{
+          width: "100%",
+        }}
+        stripe={"odd"}
+        borderAxis="none"
+      >
+        <tbody>
+          <tr>
+            <td>Maquina</td>{" "}
+            <td>
+              {name} {model}
+            </td>
+          </tr>
+          <tr>
+            <td>Ubicacion</td> <td>{selectedLocation.city}</td>
+          </tr>
+          <tr>
+            <td>Fecha de inicio</td> <td>{dates[0]}</td>
+          </tr>
+          <tr>
+            <td>Fecha de fin</td> <td>{dates[1]}</td>
+          </tr>
+          <tr>
+            <td>Duracion total</td> <td>{days} dias</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Typography sx={{ textAlign: "right", mt: 2 }} level="h3">
+        Precio total: ${price * days}
+      </Typography>
+    </>
+  );
+}
+
+export default Summary;
