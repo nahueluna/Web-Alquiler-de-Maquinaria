68df9c34e904c769fe6a7b8ca228d46ea78732f4
RamosThiago <thiagoramosnoilhan@gmail.com>
2025-05-30 20:24:44 -0300

    vite.config.js now uses loadEnv to load all the env variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: [process.env.VITE_NGROK],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      strictPort: true,
+      port: 5173,
+      allowedHosts: [env.NGROK],
+    },
+  };
 });
