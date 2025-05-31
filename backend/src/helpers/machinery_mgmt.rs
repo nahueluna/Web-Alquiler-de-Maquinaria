3feca787748b699cc1a250ad96bd9e6923edbe52
nahuel <nahuelnicolasluna@hotmail.com>
2025-05-30 22:16:11 -0300

    Added overlaping dates check and get claims functions

--- backend/src/helpers/machinery_mgmt.rs
+++ backend/src/helpers/machinery_mgmt.rs
@@ -1,5 +1,6 @@
-use crate::helpers::auth::validate_jwt;
+use crate::{custom_types::structs::Claims, helpers::auth::validate_jwt};
 use axum::{http::StatusCode, Json};
+use chrono::NaiveDate;
 use serde_json::json;
 
 pub fn validate_client(access_token: &str) -> Option<(StatusCode, Json<serde_json::Value>)> {
@@ -22,4 +23,20 @@ pub fn validate_client(access_token: &str) -> Option<(StatusCode, Json<serde_jso
     }
 
     return None;
+}
+
+pub fn get_claims_from_token(access_token: &str) -> Option<Claims> {
+    match validate_jwt(&access_token) {
+        Some(data) => Some(data.claims),
+        None => None,
+    }
+}
+
+pub fn date_is_overlap(
+    a_start: NaiveDate,
+    a_end: NaiveDate,
+    b_start: NaiveDate,
+    b_end: NaiveDate,
+) -> bool {
+    a_start <= b_end && b_start <= a_end
 }
