1a22656bf96490333c2863482a8067e5c27be4c6
Noxde <mateo.tesorieri@hotmail.com>
2025-06-03 00:38:29 -0300

    Import the css files for react-date-range

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { CssVarsProvider } from "@mui/joy/styles";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { UserProvider } from "./context/UserContext";
+import "@iroomit/react-date-range/dist/styles.css"; // main css file
+import "@iroomit/react-date-range/dist/theme/default.css"; // theme css file
 
 const App = () => {
   return (
